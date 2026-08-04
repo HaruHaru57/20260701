ae2aa260ac4e2a1335573bd1c0b95fbee20143b3
HaruHaru57 <miya1657@icloud.com>
2026-08-04 20:54:33 +0900

    34

---
+++ script.js
@@ -0,0 +1,15 @@
+const userName = "たろう";
+
+let score = 100;
+
+score = 150;
+
+console.log("--- 変数の確認 ---");
+console.log("ユーザー名", userName);
+console.log("スコア:", score);
+
+console.log(`${userName}さんの現在のスコアは ${score}点です!`);
+
+console.log("--- データ型の確認 ---");
+console.log("userNameの型:", typeof userName);
+console.log("scoreの型:", typeof score);
