f5ed3608370362db2c8ad9083b3bca5cb6d11595
HaruHaru57 <miya1657@icloud.com>
2026-08-06 21:15:35 +0900

    36

--- script.js
+++ script.js
@@ -35,57 +35,72 @@
 // player.level = 51;
 // console.log("レベルアップ後", player.level);
 
-37-2
+// 37-2
 
-const characters = [
-    {
-        id: 1,
-        name: "フリーレン",
-        job: "魔法使い",
-        level: 1000,
-        skills: ["ゾルトラーク", "花畑を出す魔法"]
-    },
-    {
-        id: 2,
-        name: "フェルン",
-        job: "魔法使い",
-        level: 18,
-        skills: ["一般攻撃魔法", "防御魔法"]
-    },
-    {
-        id: 3,
-        name: "シュタルク",
-        job: "戦士",
-        level: 19,
-        skills: ["閃天撃"]
-    }
-];
+// const characters = [
+//     {
+//         id: 1,
+//         name: "フリーレン",
+//         job: "魔法使い",
+//         level: 1000,
+//         skills: ["ゾルトラーク", "花畑を出す魔法"]
+//     },
+//     {
+//         id: 2,
+//         name: "フェルン",
+//         job: "魔法使い",
+//         level: 18,
+//         skills: ["一般攻撃魔法", "防御魔法"]
+//     },
+//     {
+//         id: 3,
+//         name: "シュタルク",
+//         job: "戦士",
+//         level: 19,
+//         skills: ["閃天撃"]
+//     }
+// ];
 
-console.log("=== 練習問題スタート ===");
+// console.log("=== 練習問題スタート ===");
 
-// Q1. 「フリーレン」の名前をコンソールに表示してください。
-// ヒント: 0番目のデータの .name
-console.log("Q1の答え:", characters[0].name);
-
-
-// Q2. 「シュタルク」の職業（job）をコンソールに表示してください。
-// ヒント: 2番目のデータの .job
-console.log("Q2の答え:", characters[2].job);
+// // Q1. 「フリーレン」の名前をコンソールに表示してください。
+// // ヒント: 0番目のデータの .name
+// console.log("Q1の答え:", characters[0].name);
 
 
-// Q3. 「フェルン」の1つ目のスキル（skillsの0番目）を表示してください。
-// ヒント: 1番目のデータの .skills[0]
-console.log("Q3の答え:", characters[1].skills[0]);
+// // Q2. 「シュタルク」の職業（job）をコンソールに表示してください。
+// // ヒント: 2番目のデータの .job
+// console.log("Q2の答え:", characters[2].job);
 
 
-// Q4. 「シュタルク」のレベル（level）を 20 に上書き（更新）し、
-//     更新後のレベルをコンソールに表示してください。
-characters[2].level = 20; 
-console.log("Q4の答え (レベル20のはず):", characters[2].level);
+// // Q3. 「フェルン」の1つ目のスキル（skillsの0番目）を表示してください。
+// // ヒント: 1番目のデータの .skills[0]
+// console.log("Q3の答え:", characters[1].skills[0]);
 
 
-// Q5. 新しいスキルを配列に追加する練習！
-//     characters[0].skills.push("民間魔法") を実行して、
-//     フリーレンのスキル一覧をコンソールで確認してみよう。
-characters[0].skills.push("民間魔法");
-console.log("Q5の答え (スキルが追加された配列):", characters[0].skills);
+// // Q4. 「シュタルク」のレベル（level）を 20 に上書き（更新）し、
+// //     更新後のレベルをコンソールに表示してください。
+// characters[2].level = 20; 
+// console.log("Q4の答え (レベル20のはず):", characters[2].level);
+
+
+// // Q5. 新しいスキルを配列に追加する練習！
+// //     characters[0].skills.push("民間魔法") を実行して、
+// //     フリーレンのスキル一覧をコンソールで確認してみよう。
+// characters[0].skills.push("民間魔法");
+// console.log("Q5の答え (スキルが追加された配列):", characters[0].skills);
+
+38
+
+const playerLevel = 50;
+const hasKey = true;
+
+console.log("--- ダンジョンの扉の判定 ---");
+
+if (playerLevel >= 50 && haskKey === true) {
+    console.log("ボス部屋の扉が開いた!");
+} else if (playerLevel >= 50 && hasKey === false) {
+    console.log("レベルは足りているが、鍵がない...");
+} else {
+    console.log("レベルが足りない!修行し直そう。");
+}
